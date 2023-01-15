ae249b48a361fe1fc9b3e31b8a0a0df3c088cda3
AnastasiiaVld <anastasiia94cherniavska@gmail.com>
2023-01-15 22:23:47 +0200

    complete homework 07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+function createImgMarkup() {
+  galleryItems.map((img) =>
+    galleryEl.insertAdjacentHTML(
+      "beforeend",
+      `<div class="gallery__item">
+                <a class="gallery__link" href="${img.original}">
+                <img
+                    class="gallery__image"
+                    src="${img.preview}"
+                    data-source="${img.original}"
+                    alt="${img.description}"
+                />
+                </a>
+            </div>`
+    )
+  );
+}
+
+createImgMarkup();
+
+const handleModalClick = (e) => {
+  e.preventDefault();
+
+  const modal = basicLightbox.create(`
+    <div class="modal">
+        <img src="${e.target.dataset.source}"></img>
+    </div>
+  `);
+
+  modal.show();
+
+  document
+    .querySelector(".modal")
+    .addEventListener("click", () => modal.close());
+
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      modal.close();
+    }
+  });
+};
+
+galleryEl.querySelectorAll(".gallery__image").forEach((image) => {
+  image.addEventListener("click", handleModalClick);
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+function createImgMarkup() {
+  galleryItems.map((img) =>
+    galleryEl.insertAdjacentHTML(
+      "beforeend",
+      `
+        <a class="gallery__item" href="${img.original}">
+            <img
+                class="gallery__image"
+                src="${img.preview}"
+                alt="${img.description}"
+                title="${img.description}"
+            />
+        </a>
+        `
+    )
+  );
+}
+
+createImgMarkup();
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
